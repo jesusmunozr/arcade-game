f91372dcc4494f2f4f4e9ef6336af245b1a1ddbe
Jesus Munoz <jesus.munozr@gmail.com>
2018-10-06 14:32:21 -0500

    Game finished!

--- js/app.js
+++ js/app.js
@@ -1,73 +1,83 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-    this.x = 0;
-    this.y = 1;
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+var Enemy = function(row, speed) {
+    this.x = -101;
+    this.y = (row * 83) - 20;
+    this.speed = speed;
     this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-
-    
+    // Detect colision and reset position of the Enemy and Player
+    if (((this.x + 101 - 30 >= player.x && this.x + 101 - 30 < player.x + 101) 
+        || (this.x >= player.x && this.x < player.x + 101)) 
+        && this.y === player.y){
+        player.reset();
+        this.reset();
+    } else { // If there is not collision Enemy continues 
+        this.x += this.speed * dt;
+        if(this.x >= 505){
+            this.x = -101;
+        }
+    }
 };
-
 // Draw the enemy on the screen, required method for gamea
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+// Reset Enemy position
+Enemy.prototype.reset = function() {
+    this.x = -101;
+}
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Player class
 var Player = function() {
-    this.x = 202;
-    this.y = 373.5;
-    this.speedX = 101;
-    this.speedY = 83;
+    this.x = 202; // Initial position in X axis
+    this.y = 395; // Initial position in Y axis
+    this.stepX = 101;
+    this.stepY = 83;
     this.movement = '';
     this.sprite = 'images/char-boy.png';
 };
+// Reset the player position
+Player.prototype.reset = function () {
+    this.x = 202;
+    this.y = 395;
+}
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 Player.prototype.update = function() {
     switch (this.movement) {
         case 'left':
-            if(this.x - this.speedX < 0)
+            if(this.x - this.stepX < 0)
                 return;
-            this.x -= this.speedX;
+            this.x -= this.stepX;
             break;
         case 'up':
-            if(this.y - this.speedY < -41.5)
+            if(this.y - this.stepY < -20)
                 return;
-            this.y -= this.speedY;
+            this.y -= this.stepY;
             break;
         case 'right':
-            if(this.x + this.speedX > 404)
+            if(this.x + this.stepX > 404)
                 return;
-            this.x += this.speedX;
+            this.x += this.stepX;
             break;
         case 'down':
-            if(this.y + this.speedY > 373.5)
+            if(this.y + this.stepY > 395)
                 return;
-            this.y += this.speedY;
+            this.y += this.stepY;
             break;
         default:
             break;
     }
 
-    if (this.y == -41.5){
-        console.log('finish');
-        this.x = 202;
-        this.y = 373.5;
+    // If player arrives to the top, position is reset to start again
+    if (this.y == -20){
+        console.log('finish!!');
+        this.reset();
     }
 
     this.movement = '';
@@ -76,12 +86,10 @@ Player.prototype.handleInput = function(mov) {
         this.movement = mov;
 }
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-let allEnemies = [];// [new Enemy(), new Enemy()];
+// Create player instance
 let player = new Player();
-
+// Create Enemies instances
+let allEnemies = [new Enemy(1, 250), new Enemy(2, 100), new Enemy(3, 100),new Enemy(1, 300), new Enemy(2, 200), new Enemy(3, 150)];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
